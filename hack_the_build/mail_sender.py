d0ea80921074727d165568895bbf0db82c6ba823
Meghanshu Verma <95580405+Dark-Awesome@users.noreply.github.com>
2023-08-16 16:19:52 +0530

    Add files via upload

---
+++ hack_the_build/mail_sender.py
@@ -0,0 +1,25 @@
+import smtplib as smt 
+
+
+def sendmail(sender_mail,sender_password,message,receiver_mail):
+    #senders id should have security on less safe app ON.
+
+    #here server s is only for gmail servers 
+    s = smt.SMTP("smtp.mail.yahoo.com", 465)
+    s.starttls
+    s.login(sender_mail, sender_password)
+    s.sendmail(sender_mail, receiver_mail , message)
+    s.quit()
+
+def send_to_all(sender_mail, sender_password,message,listofrecievers):
+    for i in range(len(listofrecievers)):
+        sendmail(sender_mail, sender_password,message,receiver_mail=listofrecievers[i])
+
+sendmail("ankitdubey.iitjmu@yahoo.com", "123456789@123456789","hello shubhu", "shubhabratabarik711@gmail.com")
+
+# Server name: smtp.mail.yahoo.com
+
+# Port: 465
+
+# SSL or TLS: ON
+
